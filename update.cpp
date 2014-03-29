45a1d7a8804edb6c97dbf7170e1ebb2c6a991f79
asdfvar <asdfvar@gmail.com>
2014-03-28 20:44:47 -0700

    added another way for the robot to move.

--- update.cpp
+++ update.cpp
@@ -2,7 +2,26 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define SPEED 0.01
+#define OMEGA 0.1
+
+#define dSPEED 0.00002
+#define dOMEGA 0.0002
+
+enum {DEF = 0, MOV};
+
+static int mode = DEF;
+static float mmove = 0.0;
+static float tturn = 0.0;
+
 int robot::update(float dt){
+
+#if 1
+   if (mode == MOV){
+      speed += mmove*dSPEED;
+      omega += tturn*dOMEGA;
+   }
+#endif
 
    posx += speed*cosf(dir)*dt;
    posy += speed*sinf(dir)*dt;
@@ -16,16 +35,36 @@ int robot::move(unsigned char key){
 
    switch (key){
       case 'w':
-         speed = 0.01;
+         if (mode == MOV)
+            mmove = 1.0;
+         else
+            speed = SPEED;
          break;
       case 'a':
-         omega = 0.1;
+         if (mode == MOV)
+            tturn = 1.0;
+         else
+            omega = OMEGA;
          break;
       case 'd':
-         omega = -0.1;
+         if (mode == MOV)
+            tturn = -1.0;
+         else
+            omega = -OMEGA;
          break;
       case 's':
-         speed = -0.01;
+         if (mode == MOV)
+            mmove = -1.0;
+         else
+            speed = -SPEED;
+         break;
+      case 'f':
+         mode = MOV;
+         break;
+      case 'j':
+         mode = DEF;
+         speed = 0.0;
+         omega = 0.0;
          break;
       case 'q':
          exit(1);
@@ -41,16 +80,28 @@ int robot::unmove(unsigned char key){
 
    switch (key){
       case 'w':
-         speed = 0.0;
+         if (mode == MOV)
+            mmove = 0.0;
+         else if (mode == DEF)
+            speed = 0.0;
          break;
       case 's':
-         speed = 0.0;
+         if (mode == MOV)
+            mmove = 0.0;
+         else if (mode == DEF)
+            speed = 0.0;
          break;
       case 'a':
-         omega = 0.0;
+         if (mode == MOV)
+            tturn = 0.0;
+         else if (mode == DEF)
+            omega = 0.0;
          break;
       case 'd':
-         omega = 0.0;
+         if (mode == MOV)
+            tturn = 0.0;
+         else if (mode == DEF)
+            omega = 0.0;
          break;
       default:
          break;
