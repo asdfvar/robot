d57c78f982e85192bacfe7e7e8b2e56e512da613
asdfvar <asdfvar@gmail.com>
2014-04-12 21:09:58 -0700

    distance from lines in the map class is a work in progress

--- map.cpp
+++ map.cpp
@@ -6,6 +6,7 @@
 #define MAP_SIZE 16384
 
 #define MIN(A,B) A < B ? A : B
+#define ABS(Z) Z < 0.0 ? -Z : Z
 
 map::map(){
 
@@ -42,7 +43,7 @@ map::~map(){
 float map::getdist(float x, float y) {
 
    int i;
-   float dist, maxdist;
+   float dist, mindist;
    bool initdone=false;
 
    // distance from circles
@@ -52,12 +53,35 @@ float map::getdist(float x, float y) {
       (y - centery[i])*(y - centery[i]));
       dist -= radii[i];
       if (!initdone) {
-         maxdist = dist;
+         mindist = dist;
          initdone = true;
       } else {
-         maxdist = MIN(maxdist, dist);
+         mindist = MIN(mindist, dist);
       }
    }
 
-   return maxdist;
+   /*
+    * distance from lines
+    * 
+    *      |(y2-y1)*x0 - (x2-x1)*y0 - x1*y2 + x2*y1|
+    *  d = -----------------------------------------
+    *           sqrt((x2-x1)^2 + (y2-y1)^2)
+    *
+    *  line defined from the points (x1,y1) and (x2,y2) and the
+    *  point is defined from (x0,y0)
+    */
+  
+   for (i=0; i<Nlines; i++) {
+      // insert check if point is within the line boundaries here
+      dist = ABS((y2[i]-y1[i])*x - (x2[i]-x1[i])*y - x1[i]*y2[i] + x2[i]*y1[i])/
+       sqrtf((x2[i]-x1[i])*(x2[i]-x1[i]) + (y2[i]-y1[i])*(y2[i]-y1[i]));
+      if (!initdone) {
+         mindist = dist;
+         initdone = true;
+      } else {
+         mindist = MIN(mindist, dist);
+      }
+   }
+std::cout << dist << std::endl;
+   return mindist;
 }
